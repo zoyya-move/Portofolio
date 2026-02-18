49ecee3e0f23b9074284e678ecea7965cf574a29
zoyya-move <raihanadyatma649@gmail.com>
2026-02-18 08:32:16 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/portofolio/",
+  base: "/",
   plugins: [react(), tailwindcss()],
 })
