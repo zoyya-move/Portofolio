12f48df79b4571c21c60f3a12861ac7f2be1c283
zoyya-move <raihanadyatma649@gmail.com>
2026-02-18 08:54:03 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -32,17 +32,6 @@ function App() {
   // -------------------------
 
   useEffect(() => {
-    const isReload =
-      performance.getEntriesByType("navigation")[0]?.type === "reload";
-
-    if (isReload) {
-      // Ambil path tanpa hash
-      const baseUrl = window.location.origin + "/portofolio/";
-      window.location.replace(baseUrl);
-    }
-  }, []);
-
-  useEffect(() => {
     const observer = new IntersectionObserver(
       ([entry]) => {
         if (entry.isIntersecting) {
